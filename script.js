ce1e80f392d525dc6e00fa5c29782e2137ede50b
Jake Pedigo <pedigojacob@gmail.com>
2023-11-13 14:12:21 -0700

    dynamic questions

--- script.js
+++ script.js
@@ -1,34 +1,23 @@
 window.onload = function() {
-  const inputs = document.querySelectorAll('input[type="text"]');
+  const inputBlock = document.getElementById('input-block');
   const textDisplay = document.getElementById('prefill-object');
   const variablesDisplay = document.getElementById('variables-object');
   const pwf = document.getElementById('prefill-pwf');
-  const prefillData = {
-    amountSeeking: null,
-    timeInBusiness: null,
-    averageMonthlySales: null,
-    creditScore: null,
-    entityType: null,
-    businessName: null,
-    first: null,
-    last: null,
-    email: null,
-    phone: null,
-  };
 
-  const prefillBody = {};
+  const prefillables = pwf.getPrefillableValues();
+  const prefillData = {};
+  Object.keys(prefillables).forEach( key => {
+    const dataType = prefillables[key];
+    prefillData[key] = null;
+    buildInput(key, dataType);
+  })
   textDisplay.innerText = JSON.stringify(prefillData, undefined, 2);
   variablesDisplay.innerText = JSON.stringify(pwf.getPrefillableValues(), undefined, 2);
 
   
   document.getElementById('prefill-test').addEventListener('click', () => {
-    Object.values(prefillData).forEach((v, i) => {
-      if (v !== null) {
-        prefillBody[Object.keys(prefillData)[i]] = v;
-      }
-    });
-    console.log('Sourced prefill data:', prefillBody);
-    pwf.setPrefill(prefillBody);
+    console.log('Sourced prefill data:', prefillData);
+    pwf.setPrefill(prefillData);
   });
   
   document.getElementById('prefill-variables').addEventListener('click', () => {
@@ -37,21 +26,45 @@ window.onload = function() {
     variablesDisplay.innerText = JSON.stringify(variables, undefined, 2);
   });
 
+  // document.getElementById('customQuestions').addEventListener('input', (e) => {
+  //   pwf.questions = JSON.parse(e.target.value);
+  // });
 
+  function buildInput(id, type) {
+    const inputContainer = document.createElement('div');
+    inputContainer.className = 'input-container';
 
-  inputs.forEach((input) => {
-    input.addEventListener('input', (e) => {
+    const inputLabel = document.createElement('label');
+    inputLabel.setAttribute('for', id);
+    inputLabel.innerText = `${camelCaseToTitle(id)}: `
 
-      const regex = /[0-9]/g
-      if(e.target.dataset.type === 'number' && regex.test(e.target.value)) {
-        prefillData[input.name] = parseInt(e.target.value);
+    const inputEl = document.createElement('input');
+    inputEl.setAttribute('type', 'text');
+    inputEl.setAttribute('id', `${id}Input`);
+    inputEl.setAttribute('name', id);
+    inputEl.setAttribute('data-type', type);
+
+    inputEl.addEventListener('input', (e) => {
+      const regex = /[0-9]/g;
+      if (type === 'number' && regex.test(e.target.value)) {
+        prefillData[id] = parseInt(e.target.value);
       } else if (e.target.value.length > 0) {
-        prefillData[input.name] = e.target.value;
+        prefillData[id] = e.target.value;
       } else {
-        prefillData[input.name] = null;
+        prefillData[id] = null;
       }
 
       textDisplay.innerText = JSON.stringify(prefillData, undefined, 2);
     });
-  });
+
+    inputContainer.appendChild(inputLabel);
+    inputContainer.appendChild(inputEl);
+    inputBlock.appendChild(inputContainer);
+  }
+  
+  function camelCaseToTitle(val) {
+    return val.replace(/([A-Z])/g, (match) => ` ${match}`)
+      .replace(/^./, (match) => match.toUpperCase())
+      .trim();
+  };
 }
